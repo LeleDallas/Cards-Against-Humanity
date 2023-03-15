7d86b794d05be2a175d79ccdbd0dfe9ebe101317
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-15 18:26:04 +0100

    chore: update vitest config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    coverage: {
+      provider: "c8",
+      all: true,
+      exclude: ["src/**/*.main.tsx"],
+      include: ["src/**/*.tsx"],
+    },
+  },
+});
