865d43d7185d99e065e0c1517fac064f3757aade
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-20 10:48:45 +0100

    fix: test coverage

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,14 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    setupFiles: [resolve(__dirname, './setup.js')],
     environment: "jsdom",
     coverage: {
       provider: "c8",
