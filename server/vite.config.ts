75afe4a2e431c898c59fa2ad712334c315746f00
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-22 17:38:50 +0100

    chore: update test coverage

---
+++ server/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      all: true,
+      include: ["**/*.ts"],
+    },
+  },
+});
