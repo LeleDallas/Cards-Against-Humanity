eccd14dbd4c0fc01b8797b69476751c8b0931cac
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-31 20:07:44 +0200

    feat: update route with theme

--- client/src/route/AppRoute.tsx
+++ client/src/route/AppRoute.tsx
@@ -3,14 +3,23 @@ import SocketContextComponent from "../context/SocketContextComponent";
 import Home from "../components/Home/Home";
 import App from "../App";
 import WaitingLobby from "../components/Lobby/WaitingLobby";
+import { ConfigProvider } from "antd";
 
-const AppRoute = (props:any) => {
-    return <SocketContextComponent>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/lobby" element={<App />} />
-            <Route path="/waiting" element={<WaitingLobby {...props} />} />
-        </Routes>
-    </SocketContextComponent>
-}
+const AppRoute = (props: any) =>
+    <ConfigProvider
+        theme={{
+            token: {
+                colorPrimary: '#000',
+            },
+        }}
+    >
+        <SocketContextComponent>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/lobby" element={<App />} />
+                <Route path="/waiting" element={<WaitingLobby {...props} />} />
+            </Routes>
+        </SocketContextComponent>
+    </ConfigProvider>
+
 export default AppRoute
