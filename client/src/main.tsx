ab57a1bf5ff0d339caedc87f1f45cef3226a5d19
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-31 15:04:07 +0200

    feat: update main route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import SocketContextComponent from './context/SocketContextComponent';
+import AppRoute from "./route/AppRoute";
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <SocketContextComponent>
-    <App />
-  </SocketContextComponent>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRoute/>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
