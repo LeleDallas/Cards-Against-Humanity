f961f7e8b3ba1568588fcbacac5c764be01d350a
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-03 14:13:47 +0200

    refactor: mobile home style

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -9,20 +9,23 @@ import jazzHands from '../../assets/jazzhands.png';
 import berry from '../../assets/miracleberry.png';
 import airplane from '../../assets/paperairplane.png';
 import pizza from '../../assets/pizza.png';
+import { isMobile } from "react-device-detect";
 
 const ContainerWhite = styled.div`
 position: absolute;
 transform: translate(-50%, -50%);
 top: 40%;
-left: 50%;
+left: ${isMobile ? "40%" : "50%"};
 `
+
 const ContainerBlack = styled.div`
 position: absolute;
 transform: translate(-50%, -50%);
-top: 50%;
-left: 53%;
+top: ${isMobile ? "30%" : "50%"};
+left: ${isMobile ? "34%" : "53%"};
 perspective: 50em;
-rotate: 22deg
+${isMobile && 'transform: rotate(18deg)'};
+rotate: 22deg;
 `
 
 const Home = () => {
@@ -35,19 +38,21 @@ const Home = () => {
             <ContainerBlack style={{ rotate: "22deg" }}>
                 <BlackCard />
             </ContainerBlack>
-            <Row style={{ position: "absolute", bottom: "18%", width: "50%", textAlign: "center", left: "25%" }} justify="space-between">
-                <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/lobby")}>Join room</Button>
-                <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/create")}>Create room</Button>
-                <Button style={{ width: 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/rules")}>Rules</Button>
+            <Row align="middle"
+                gutter={[0, 16]}
+                style={{ position: "absolute", bottom: "18%", width: "50%", textAlign: "center", left: 0, right: 0, margin: "auto" }}
+                justify="space-between"
+            >
+                <Button style={{ width: isMobile ? 250 : 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/lobby")}>Join room</Button>
+                <Button style={{ width: isMobile ? 250 : 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/create")}>Create room</Button>
+                <Button style={{ width: isMobile ? 250 : 150, height: 50, fontSize: 18 }} size="large" onClick={() => navigate("/rules")}>Rules</Button>
             </Row>
-
-            <img style={{ width: "12em", position: "absolute", top: 40, left: 20 }} src={pizza} alt="pizza" />
-            <img style={{ width: "9em", position: "absolute", top: "30%", left: 20 }} src={berry} alt="berry" />
-            <img style={{ width: "12em", position: "absolute", bottom: 40, left: 20 }} src={fartAndWalkaway} alt="fartAndWalkaway" />
-            <img style={{ width: "12em", position: "absolute", top: 40, right: 20 }} src={airplane} alt="airplane" />
-            <img style={{ width: "12em", position: "absolute", top: "40%", right: 20 }} src={jazzHands} alt="jazzHands" />
-            <img style={{ width: "12em", position: "absolute", bottom: 40, right: 20 }} src={cowboyBoot} alt="cowboyBoot" />
-
+            <img style={{ width: isMobile ? "8em" : "12em", position: "absolute", top: 40, left: 20 }} src={pizza} alt="pizza" />
+            {!isMobile && <img style={{ width: "9em", position: "absolute", top: "30%", left: 20 }} src={berry} alt="berry" />}
+            <img style={{ width: isMobile ? "8em" : "12em", position: "absolute", bottom: 40, left: 20 }} src={fartAndWalkaway} alt="fartAndWalkaway" />
+            <img style={{ width: isMobile ? "8em" : "12em", position: "absolute", top: 40, right: 20 }} src={airplane} alt="airplane" />
+            {!isMobile && <img style={{ width: "12em", position: "absolute", top: "40%", right: 20 }} src={jazzHands} alt="jazzHands" />}
+            <img style={{ width: isMobile ? "8em" : "12em", position: "absolute", bottom: 40, right: isMobile ? 16 : 20 }} src={cowboyBoot} alt="cowboyBoot" />
         </div >
     )
 }
