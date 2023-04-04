fab427fc8a9f0b72a4f4898189f5f1412f18f58b
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-05 00:18:25 +0200

    test: update White Card

--- client/test/WhiteCard.test.tsx
+++ client/test/WhiteCard.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { expect, it, describe, assert } from 'vitest'
+import { expect, it, describe } from 'vitest'
 import { render } from '@testing-library/react';
 import WhiteCard from '../src/components/Cards/WhiteCard';
 
