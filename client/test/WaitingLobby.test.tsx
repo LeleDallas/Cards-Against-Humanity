f37cee6e73c47a3b1320689af9bbc92b40738722
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-05 00:41:58 +0200

    test: Waiting Lobby

---
+++ client/test/WaitingLobby.test.tsx
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { expect, it, describe, vi } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom';
+import WaitingLobby from '../src/components/Lobby/WaitingLobby';
+
+const mockedUseNavigate = vi.fn();
+vi.mock("react-router-dom", async () => {
+    const mod = await vi.importActual<typeof import("react-router-dom")>(
+        "react-router-dom"
+    );
+    return {
+        ...mod,
+        useNavigate: () => mockedUseNavigate,
+    };
+});
+
+describe('Waiting Lobby component', () => {
+
+    it('renders without errors', () => {
+        const { getByText } = render(
+            <BrowserRouter>
+                <WaitingLobby />
+            </BrowserRouter>
+        )
+        expect(getByText(/Back/)).toBeInTheDocument()
+        expect(getByText(/Go to the homepage/)).toBeInTheDocument()
+    })
+
+    it('fire buttons event', () => {
+        const { getByText } = render(
+            <BrowserRouter>
+                <WaitingLobby />
+            </BrowserRouter>
+        )
+
+        const start = getByText(/Go to the homepage/)
+        fireEvent.click(start)
+        expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
+    })
+})
