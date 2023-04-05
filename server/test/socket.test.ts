877138bb9281e8b636ef526490feafdb848aaf08
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-05 11:10:22 +0200

    test: socket update

--- server/test/socket.test.ts
+++ server/test/socket.test.ts
@@ -1,8 +1,8 @@
 import http from 'http';
 import { io as Client } from "socket.io-client";
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import { Server as HttpServer } from 'http';
-import { expect, test, describe, beforeAll, afterAll, beforeEach, it, afterEach } from 'vitest'
+import { expect, test, describe, beforeAll, afterAll, beforeEach, it } from 'vitest'
 import dotenv from 'dotenv';
 import { ServerSocket } from '../socket';
 
@@ -53,9 +53,10 @@ describe("Default socket test", () => {
 
 describe('Server Socket', () => {
     let serverSocket: ServerSocket;
+    let httpServer: HttpServer;
 
     beforeEach(() => {
-        const httpServer: HttpServer = {} as HttpServer;
+        httpServer = {} as HttpServer;
         serverSocket = new ServerSocket(httpServer);
     });
 
@@ -81,7 +82,58 @@ describe('Server Socket', () => {
         const name = 'test_event';
         const payload = { message: 'Hello, World!' };
         const users = ['1234', '5678'];
-        serverSocket.sendMessage(name, users, payload); 
+        serverSocket.sendMessage(name, users, payload);
+    });
 
+
+    it('should return filtered rooms when getRooms is called', () => {
+        const roomId1 = 'room1';
+        const roomId2 = 'room2';
+        const user1 = 'user1';
+        const user2 = 'user2';
+        const user3 = 'user3';
+        const availableRooms = new Map<string, Set<string>>([
+            [roomId1, new Set([user1, user2])],
+            [roomId2, new Set([user3])]
+        ]);
+
+        (serverSocket.io.sockets.adapter as any).rooms = availableRooms;
+        const result = serverSocket.getRooms();
+        expect(result).toEqual(new Map([
+            [roomId1, [user1, user2]],
+            [roomId2, [user3]]
+        ]));
+    });
+
+    it('should return the current room when getCurrentRoom is called', () => {
+        const lobbyName = 'room1';
+        const user1 = 'user1';
+        const user2 = 'user2';
+        const room = new Set([user1, user2]);
+        (serverSocket.io.sockets.adapter as any).rooms.set(lobbyName, room);
+
+        const result = serverSocket.getCurrentRoom(lobbyName);
+        expect(result).toEqual({
+            data: {
+                lobbyName,
+                users: JSON.stringify([...room])
+            }
+        });
+    });
+
+    it('should return the UID from the socket ID when getUidFromSocketID is called', () => {
+        const uid1 = '123';
+        const uid2 = '456';
+        serverSocket.users = {
+            [uid1]: 'socket1',
+            [uid2]: 'socket2'
+        };
+
+        const result1 = serverSocket.getUidFromSocketID('socket1');
+        const result2 = serverSocket.getUidFromSocketID('socket2');
+        const result3 = serverSocket.getUidFromSocketID('socket3');
+        expect(result1).toEqual(uid1);
+        expect(result2).toEqual(uid2);
+        expect(result3).toBeUndefined();
     });
 });
