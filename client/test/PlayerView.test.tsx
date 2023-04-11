a970304598a295d918d14d7fcead7f379a744286
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-11 16:36:37 +0200

    test: update player

--- client/test/PlayerView.test.tsx
+++ client/test/PlayerView.test.tsx
@@ -1,11 +1,19 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { expect, it, describe, vi } from 'vitest'
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import PlayerView from '../src/pages/Game/PlayerView';
 import { store } from '../src/store/store';
 import { Provider } from 'react-redux';
+
+vi.mock("react-redux", async () => {
+    const actual: any = await vi.importActual("react-redux")
+    return {
+        ...actual,
+        useDispatch: vi.fn(),
+    }
+})
 
 const mockedUseNavigate = vi.fn();
 vi.mock("react-router-dom", async () => {
@@ -18,7 +26,7 @@ vi.mock("react-router-dom", async () => {
     };
 });
 
-describe('Czar View', () => {
+describe('Player View', () => {
     it('renders view correctly', () => {
         const { getByText } = render(
             <Provider store={store}>
@@ -29,22 +37,5 @@ describe('Czar View', () => {
         );
         expect(getByText("Submit Response")).toBeInTheDocument()
     });
-
-    it('select a card on click', () => {
-        const { getByText } = render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <PlayerView />
-                </BrowserRouter>
-            </Provider>
-        );
-        // const card = getByText("1")
-        // expect(card).toBeInTheDocument()
-        // fireEvent.click(card)
-        // expect(card.style)
-
-    });
-
-
 
 });
