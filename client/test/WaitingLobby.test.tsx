42bcf0a10a24a5363e64a20ee34c6bcd980e656f
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-12 11:57:28 +0200

    test: update Waiting Lobby

--- client/test/WaitingLobby.test.tsx
+++ client/test/WaitingLobby.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { expect, it, describe, vi } from 'vitest'
+import { expect, it, describe, vi, test } from 'vitest'
 import { fireEvent, render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom';
 import WaitingLobby from '../src/pages/Lobby/WaitingLobby';
@@ -57,38 +57,115 @@ describe('Waiting Lobby component', () => {
         fireEvent.click(back)
         expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
     })
-
-    // it('calls leaveRoom function on click', () => {
-    //     const leaveRoom = vi.fn();
-    //     const navigate = vi.fn();
-    //     const socketState = { socket: {}, rooms: [] };
-    //     const location = { state: { type: 'user', roomName: 'Test Room' } };
-    //     const { getByRole } = render(
-    //         <BrowserRouter>
-    //             <SocketContextProvider value={socketState}>
-    //                 <WaitingLobby navigate={navigate} leaveRoom={leaveRoom} location={location} />
-    //             </SocketContextProvider>
-    //         </BrowserRouter>
-    //     );
-    //     const backButton = getByRole('button');
-    //     fireEvent.click(backButton);
-    //     expect(leaveRoom).toHaveBeenCalledWith(socketState.socket, location.state.roomName, navigate);
-    // });
-
-    // test('calls deleteRoom function on confirm', () => {
-    //     const deleteRoom = jest.fn();
-    //     const navigate = jest.fn();
-    //     const socketState = { socket: {}, rooms: [] };
-    //     const location = { state: { type: 'admin', roomName: 'Test Room' } };
-    //     const { getByRole } = render(
-    //         <BrowserRouter>
-    //             <WaitingLobby socketState={socketState} navigate={navigate} deleteRoom={deleteRoom} location={location} />
-    //         </BrowserRouter>
-    //     );
-    //     const backButton = getByRole('button');
-    //     fireEvent.click(backButton);
-    //     const confirmButton = getByRole('button', { name: 'Yes' });
-    //     fireEvent.click(confirmButton);
-    //     expect(deleteRoom).toHaveBeenCalledWith(socketState.socket, location.state.roomName, navigate);
-    // });
 })
+
+
+describe("WaitingLobby", () => {
+    const state = { roomName: "test-room", type: "admin" };
+    const navigate = vi.fn();
+    const deleteRoom = vi.fn();
+    const leaveRoom = vi.fn();
+    const startGame = vi.fn();
+    const socket = { on: vi.fn(), emit: vi.fn() };
+    const socketState = { socket, rooms: { [state.roomName]: [] } };
+    const socketContext = { socketState };
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    test('should first', () => {
+
+
+        it("renders a back button", () => {
+            const { getByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            const backButton = getByText("Back");
+
+            expect(backButton).toBeInTheDocument();
+        });
+
+
+        it("calls leaveRoom when back button is clicked", () => {
+            const { getByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            const backButton = getByText("Back");
+
+            fireEvent.click(backButton);
+
+            expect(leaveRoom).toHaveBeenCalledWith(socket, state.roomName, navigate);
+        });
+
+        it("calls deleteRoom when back button is clicked and user is admin", () => {
+            const { getByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state: { ...state, type: "user" }, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            const backButton = getByText("Back");
+
+            fireEvent.click(backButton);
+
+            expect(deleteRoom).not.toHaveBeenCalled();
+
+            render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            fireEvent.click(backButton);
+
+            expect(deleteRoom).toHaveBeenCalledWith(socket, state.roomName, navigate);
+        });
+
+        it("renders a start game button when user is admin and room has players", () => {
+            const { getByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            expect(getByText("Start Game")).toBeInTheDocument();
+        });
+
+        it("calls startGame when start game button is clicked", () => {
+            const { getByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+
+            const startGameButton = getByText("Start Game");
+
+            fireEvent.click(startGameButton);
+
+            expect(startGame).toHaveBeenCalledWith(socket, state.roomName, navigate);
+        });
+
+        it("does not render a start game button when user is not admin or room does not have players", () => {
+            const { queryByText } = render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state: { ...state, type: "user" }, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+            expect(queryByText("Start Game")).not.toBeInTheDocument();
+
+            render(
+                <BrowserRouter>
+                    <WaitingLobby {...{ state: { ...state, roomName: "other-room" }, navigate, socketContext }} />
+                </BrowserRouter>
+            );
+            expect(queryByText("Start Game")).not.toBeInTheDocument();
+        });
+    })
+});
