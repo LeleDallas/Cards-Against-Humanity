59a9ee1e093faec79cbc9e288b29a5ac25dccf4e
Piccio98 <104207695+Piccio98@users.noreply.github.com>
2023-04-13 14:42:11 +0200

    feat: handle next turn for player

--- client/src/Pages/Game/PlayerView.tsx
+++ client/src/Pages/Game/PlayerView.tsx
@@ -4,16 +4,20 @@ import WhiteCard from "../../components/Cards/WhiteCard"
 import { useContext, useEffect, useState } from "react"
 import { useAppSelector } from "../../hooks/hooks"
 import { Cards } from "../../types/cards"
-import { drawWhiteCards, sendWhiteResponse } from "../../hooks/functions"
+import { deleteRoom, drawWhiteCards, resetWhite, sendWhiteResponse, startGame } from "../../hooks/functions"
 import socketContext from "../../context/SocketContext"
 import { LoadingOutlined } from "@ant-design/icons"
+import { useNavigate } from "react-router-dom"
 
-const PlayerView = () => {
-    const { socket, black_card, czarSocketId } = useContext(socketContext).socketState;
+const PlayerView = ({ ...props }) => {
+    const { socket, black_card, czarSocketId, new_turn, rooms } = useContext(socketContext).socketState;
     const [selected, setSelected] = useState<string>("")
     const [playerHand, setPlayerHand] = useState<Array<Cards>>([])
     const white = useAppSelector(state => state?.whiteCards?.cards)
     const spin = <LoadingOutlined style={{ fontSize: 100 }} spin />;
+    const [hasPlayed, setHasPlayed] = useState<boolean>(false)
+    const { roomName } = props
+    const navigate = useNavigate()
 
     const useSelect = (cardTitle: string) => cardTitle === selected ? setSelected("") : setSelected(cardTitle)
 
@@ -22,11 +26,25 @@ const PlayerView = () => {
         setPlayerHand((oldHand: Array<Cards>) => [...oldHand, ...drawWhiteCards(white, 10)]);
     }, [])
 
+    useEffect(() => {
+        setHasPlayed(new_turn)
+        if (new_turn == false){
+            resetWhite(socket, czarSocketId)
+        }
+    }, [new_turn])
+
+    useEffect(() => {
+        if (rooms[roomName] === undefined){
+            deleteRoom(socket, roomName, navigate)
+        }
+    }, [rooms])
+
     const drawNew = () => {
-        sendWhiteResponse(socket!, czarSocketId, selected)
+        sendWhiteResponse(socket!, czarSocketId, selected, socket!.id)
         setPlayerHand(playerHand.filter((card, _) => card.title !== selected))
         setPlayerHand((oldHand: Array<Cards>) => [...oldHand, ...drawWhiteCards(white, 1)]);
         setSelected("")
+        setHasPlayed(true)
     }
 
     return (
@@ -50,7 +68,7 @@ const PlayerView = () => {
                     />
                 }
             </Row>
-            <Row justify="center" align="middle" gutter={[32, 32]} style={{ marginTop: 12 }}>
+            {!hasPlayed && <Row justify="center" align="middle" gutter={[32, 32]} style={{ marginTop: 12 }}>
                 {playerHand.map((card, index) =>
                     <Col key={index} onClick={() => useSelect(card.title)}>
                         <WhiteCard
@@ -63,7 +81,7 @@ const PlayerView = () => {
                     </Col>
                 )}
                 <Button disabled={selected === ""} type="primary" onClick={() => drawNew()}>Submit Response</Button>
-            </Row >
+            </Row >}
         </>
 
     )
