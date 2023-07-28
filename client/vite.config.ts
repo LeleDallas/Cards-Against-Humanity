241ff950afcaf16132b366287cdb112689dc9067
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-07-28 22:23:50 +0200

    fix: tsconfig path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     environment: "jsdom",
